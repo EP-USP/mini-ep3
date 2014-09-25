763d2cd9e6c43abffbf154d8631ac5e55e1e9ce8
holypriest <chemistoid@gmail.com>
2014-09-25 14:18:47 -0700

    Updated functions.py to a functional version (I am really funny!) and removed regexes.re

--- functions.py
+++ functions.py
@@ -1,22 +1,39 @@
-in_file = open('dns.txt', 'r')
+import re
+
+def copy_header(infile, out_file):
+    in_file = open(infile, 'r')
+    ns_line = re.compile('\s+NS\s+')
+    for line in in_file:
+        if ns_line.search(line):
+            in_file.close()
+            return
+        out_file.write(line)
+    in_file.close()
+
+def copy_ns(infile, out_file):
+    in_file = open(infile, 'r')
+    ns_line = re.compile('\s+NS\s+')
+    for line in in_file:
+        if ns_line.search(line):
+            out_file.write(line)
+    in_file.close()
+
+# Script starts here
 out_file = open('reverse_dns.txt', 'w')
 
-def is_ns(line):
-	return line.find('NS')
+a_line = re.compile('\s+A\s+')
+ip_pattern = re.compile('(\d+\.){3}\d+')
+domain_pattern = re.compile('^[^\s]+')
 
-def ns_position(in_file):
-	line_num = 0
-	for line in in_file:
-		line_num++
-		match = line.find('NS')
-		if match:
-			return line_num
-	return -1
+copy_header('dns.txt', out_file)
+copy_ns('dns.txt', out_file)
 
-def copy_header(in_file, out_file):
-    out_file.writelines(line for line in in_file while not is_ns(line))
-
-def copy_ns(in_file, out_file):
-	for line in in_file:
-		if is_ns(line):
-			out_file.writelines(line)
+# Gets domain and ip from A lines
+in_file = open('dns.txt', 'r')
+for line in in_file:
+    if a_line.search(line):
+        domain = domain_pattern.search(line)
+        ip = ip_pattern.search(line)
+        
+        print(domain.group(), end=' ')
+        print(ip.group())
--- regexes.re
+++
@@ -1,3 +0,0 @@
-If match, it is an A line:   \s+A\s+
-Matches IP number:   (\d+\.){3}\d+
-Matches domain in an A line:   ^[^\s]+ 

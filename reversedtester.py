b8c6f5b0fca50f6fd80a947c7a75c9a6183243a4
Ian Carvalho <iancarv@gmail.com>
2014-09-26 01:52:19 -0300

    finished some tests

--- reversedtester.py
+++ reversedtester.py
@@ -6,13 +6,14 @@ from testes.tester import Tester
 class ReverseTester(Tester):
 
     def __init__(self):
-        teste = BandecoTest()
+        teste = Teste()
+        self.testes = []
         self.set_tests()
 
     def set_tests(self):
         empty_file = DNSReverser()
-        empty_file_test = Teste('', '')
-        self.testes.append(empty_file)
+        empty_file_test = Teste('', '', 'empty_file_test')
+        self.testes.append(empty_file_test)
 
         reverser = DNSReverser('dns.txt')
         header_test = Teste(reverser.header(), '$TTL 3D\n\
@@ -22,27 +23,29 @@ todays serial #\n\
                         8H              ; refresh, seconds\n\
                         2H              ; retry, seconds\n\
                         4W              ; expire, seconds\n\
-                        1D )            ; minimum, seconds')
+                        1D )            ; minimum, seconds\n\n',
+                            'header_test')
         self.testes.append(header_test)
 
-        ns_test = Teste(reverser.ns(), '                NS      land-5.com.')
+        ns_test = Teste(reverser.ns(), '                NS      land-5.com.\n',
+                        'ns_test')
         self.testes.append(ns_test)
         reverse_test = Teste(reverser.a(), '1               PTR     router\n\
-        2               PTR     land-5.com.\n\
-        3               PTR     ns\n\
-        192             PTR     www\n\
-        2               PTR     funn\n\
-        200             PTR     ws-177200\n\
-        201             PTR     ws-177201\n\
-        202             PTR     ws-177202\n\
-        203             PTR     ws-177203\n\
-        204             PTR     ws-177204\n\
-        205             PTR     ws-177205\n\
-        250             PTR     ws-177250\n\
-        251             PTR     ws-177251\n\
-        252             PTR     ws-177252\n\
-        253             PTR     ws-177253\n\
-        254             PTR     ws-177254')
+2               PTR     land-5.com.\n\
+3               PTR     ns\n\
+192             PTR     www\n\
+2               PTR     funn\n\
+200             PTR     ws-177200\n\
+201             PTR     ws-177201\n\
+202             PTR     ws-177202\n\
+203             PTR     ws-177203\n\
+204             PTR     ws-177204\n\
+205             PTR     ws-177205\n\
+250             PTR     ws-177250\n\
+251             PTR     ws-177251\n\
+252             PTR     ws-177252\n\
+253             PTR     ws-177253\n\
+254             PTR     ws-177254\n', 'reverse_test')
         self.testes.append(reverse_test)
 
 
--- testes/teste.py
+++ testes/teste.py
@@ -1,17 +1,23 @@
 class FalhouNoTeste(Exception):
-	pass
+    pass
+
 
 class Teste(object):
-	"Classe que representa um teste generico"
+    "Classe que representa um teste generico"
+    def __init__(self, saida_obtida='', saida_esperada='', test_name=''):
+        self.test_name = test_name
+        self.saida_obtida = saida_obtida
+        self.saida_esperada = saida_esperada
 
-	def run(self):
-		if self.saida_obtida() != self.saida_esperada():
-			raise FalhouNoTeste
+    def run(self):
+        if self.obter_saida() != self.esperada():
+            print(self.test_name)
+            print(self.obter_saida())
+            print(self.esperada())
+            raise FalhouNoTeste
 
-	def saida_obtida(self):
-		return ''
+    def obter_saida(self):
+        return self.saida_obtida
 
-	def saida_esperada(self):
-		return ''
-
-
+    def esperada(self):
+        return self.saida_esperada
